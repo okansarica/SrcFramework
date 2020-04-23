7747b046317b59760236c995141bb128566fe442
okansarica <okansarica@gmail.com>
2020-04-23 13:54:35 +0100

    no message

---
+++ SrcFramework.Core/PagedList.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SrcFramework.Core
+{
+    public class PagedList<T> where T:class
+    {
+        public int TotalPageCount { get; set; }
+        public List<T> List { get; set; }
+    }
+}
