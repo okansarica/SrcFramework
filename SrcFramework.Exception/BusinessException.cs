d6b3163994e9c01cbbc5854cc48db9c7663c89a2
Okan <okansarica@gmail.com>
2022-05-03 14:43:34 +0100

    updated business exception

--- SrcFramework.Exception/BusinessException.cs
+++ SrcFramework.Exception/BusinessException.cs
@@ -1,14 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace SrcFramework.Exception
 {
     public class BusinessException:System.Exception
     {
+        public List<string> Parameters { get; set; }
         public BusinessException(string message):base(message)
         {
             
         }
+
+        public BusinessException(string message, string parameter) : base(message)
+        {
+            Parameters = new List<string> { parameter };
+        }
+
+        public BusinessException(string message, List<string> parameters) : base(message)
+        {
+            Parameters = parameters;
+        }
+
     }
 }
